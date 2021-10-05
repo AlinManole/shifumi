b1053354b4b202acceb49834053172c4db0dc9ad
Alin Manole <alin.manole@yahoo.com>
2021-10-05 10:18:43 +0200

    10.18

--- index.js
+++ index.js
@@ -1,56 +1,51 @@
 
-  
-// var rock = function rockClick() {
-//     return 1
-//  }
-
-//  var rock = 1
-
-//  console.log(rock)
-// var paper =  function paperClick(){
-//     return 2
-// }
-
-// var paper = 2
-// console.log(paper)
-// var scissors = function scissorsClick(){
-//     return 3
-// }
-
-// var scissors = 3
-// console.log(scissors)
-
-
-
-// Game 
-rock = 1;
-paper = 2;
-scissors = 3;
-
-var gamer  = rock
-
-min = 1
-max = 3
-
-if ( rock || paper || scissors) {
- var computer = Math.floor(Math.random() * (max - min + 1) + min)
-    if ( computer === 1) {
-        console.log("Computer chose Rock")
-    } else if ( computer === 2) {
-        console.log("Computer chose Paper")
-    } else {
-        console.log("Computer chose Scissors")
-    }
-
-    
-    if ( gamer === computer ) {
-        console.log("It`s a Tie !")     
-    } else if ( gamer > computer ) { 
-        console.log("Scissors cut Paper")
-    } else if ( gamer < computer ) {
-        console.log("Paper covers Rock")
-    } else if ( gamer > computer ) {
-        console.log("Rock smash scissors")
-    }
-        
+var rock;
+function rockClick() {
+    rock = 1
+    clickButon();
 }
+
+var paper;
+function paperClick(){
+    paper = 2
+    clickButon();
+}
+
+var scissors;
+function scissorsClick(){
+    scissors = 3
+    clickButon();
+}
+
+
+
+function clickButon() {
+
+    var min = 1
+    var max = 3
+    if ( rock || paper || scissors) {
+        var computer = Math.floor(Math.random() * (max - min + 1) + min)
+        if ( computer === 1) {
+            console.log("Computer chose Rock")
+        } else if ( computer === 2) {
+            console.log("Computer chose Paper")
+        } else {
+            console.log("Computer chose Scissors")
+        }
+        
+        var gamerChoice
+           
+        if ( gamerChoice === computer ) {
+            console.log("It`s a Tie !")     
+        } else if ( gamerChoice > computer ) { 
+            console.log("Scissors cut Paper")
+        } else if ( gamerChoice < computer ) {
+            console.log("Paper covers Rock")
+        } else if ( gamerChoice > computer ) {
+            console.log("Rock smash scissors")
+        }
+            
+    } //global if 
+
+
+} //clickButon close 
